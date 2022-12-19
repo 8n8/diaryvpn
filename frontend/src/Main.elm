0a3e7b16957054c6546f00e214e5e36e3c320bae
True Ghiassi <true@ghiassitrio.co.uk>
2022-12-19 22:23:28 +0000

    Basic Elm app

---
+++ frontend/src/Main.elm
@@ -0,0 +1,58 @@
+module Main exposing (main)
+
+
+import Browser
+import Url exposing (Url)
+import Browser.Navigation
+import Html
+
+
+main =
+    { init = init
+    , view = view
+    , update = update
+    , subscriptions = subscriptions
+    , onUrlRequest = onUrlRequest
+    , onUrlChange = onUrlChange
+    }
+        |> Browser.application
+
+
+type Model
+    = Model
+
+
+init : () -> Url -> Browser.Navigation.Key -> (Model, Cmd Msg)
+init _ _ _ =
+    (Model, Cmd.none)
+
+
+view : Model -> Browser.Document Msg
+view Model =
+    { title = "hello Elm!"
+    , body = [Html.text "hello Elm!"]
+    }
+
+
+type Msg
+    = Msg
+
+
+update : Msg -> Model -> (Model, Cmd Msg)
+update Msg Model =
+    (Model, Cmd.none)
+
+
+subscriptions : Model -> Sub Msg
+subscriptions Model =
+    Sub.none
+
+
+onUrlRequest : Browser.UrlRequest -> Msg
+onUrlRequest _ =
+    Msg
+
+
+onUrlChange : Url -> Msg
+onUrlChange _ =
+    Msg
