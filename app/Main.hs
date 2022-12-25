dcd1b44864d19a4ea6a4e77216757eaaffd6ab42
True Ghiassi <true@ghiassitrio.co.uk>
2022-12-25 22:40:01 +0000

    Added summaries route to backend

--- app/Main.hs
+++ app/Main.hs
@@ -1,24 +1,17 @@
 module Main (main) where
 
 import qualified Web.Scotty
-import qualified Diary
-import qualified Db
 
 main :: IO ()
 main =
+  Web.Scotty.scotty 8080 $
     do
-    db <- Db.initDb
-
-    Web.Scotty.scotty 8080 $
+      Web.Scotty.get "/" $
         do
+          Web.Scotty.setHeader "Content-Type" "text/html"
+          Web.Scotty.file "index.html"
 
-        Web.Scotty.get "/" $
-            do
-            Web.Scotty.setHeader "Content-Type" "text/html"
-            Web.Scotty.file "index.html"
-
-        Web.Scotty.get "/summaries" $
-            do
-            Web.Scotty.setHeader "Content-Type" "application/octet-stream"
-            summaries <- liftIO $ Db.query Diary.summaries db
-            Web.Scotty.raw summaries
+      Web.Scotty.get "/summaries" $
+        do
+          Web.Scotty.setHeader "Content-Type" "application/json"
+          Web.Scotty.file "summaries.json"
