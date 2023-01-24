870b5326e96a8e58db1c85ca266fb822b94946c9
True Ghiassi <true@ghiassitrio.co.uk>
2023-01-24 20:50:33 +0000

    Simplification, with no tests

--- frontend/src/Main.elm
+++ frontend/src/Main.elm
@@ -1,7 +1,51 @@
 module Main exposing (main)
 
-import Html
+import Browser
+import Browser.Navigation
+import Html exposing (Html)
+import Url exposing (Url)
 
 
 main =
-    Html.text "hi"
+    Browser.application
+        { init = init
+        , update = update
+        , subscriptions = \_ -> Sub.none
+        , onUrlChange = \_ -> ()
+        , onUrlRequest = \_ -> ()
+        , view = view
+        }
+
+
+init : () -> Url -> Browser.Navigation.Key -> ( Model, Cmd () )
+init () _ _ =
+    ( Model, Cmd.none )
+
+
+type Model
+    = Model
+
+
+update : () -> Model -> ( Model, Cmd () )
+update _ _ =
+    ( Model, Cmd.none )
+
+
+view : Model -> Browser.Document ()
+view model =
+    { title = "DiaryVPN"
+    , body = body model
+    }
+
+
+body : Model -> List (Html ())
+body _ =
+    [ Html.form
+        []
+        [ Html.div
+            []
+            [ Html.textarea [] []
+            , Html.label [] [ Html.text "Type your diary entry here:" ]
+            ]
+        ]
+    ]
