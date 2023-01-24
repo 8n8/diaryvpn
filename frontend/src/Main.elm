d8d81e9138c0e46d056644d305ca3a17bb978870
True Ghiassi <true@ghiassitrio.co.uk>
2023-01-24 21:14:51 +0000

    The text box is working

--- frontend/src/Main.elm
+++ frontend/src/Main.elm
@@ -12,37 +12,47 @@ main =
         { init = init
         , update = update
         , subscriptions = \_ -> Sub.none
-        , onUrlChange = \_ -> ()
-        , onUrlRequest = \_ -> ()
+        , onUrlChange = \_ -> NoOp
+        , onUrlRequest = \_ -> NoOp
         , view = view
         }
 
 
-init : () -> Url -> Browser.Navigation.Key -> ( String, Cmd () )
+type Msg
+    = DiaryBoxChange String
+    | NoOp
+
+
+init : () -> Url -> Browser.Navigation.Key -> ( String, Cmd Msg )
 init () _ _ =
     ( "", Cmd.none )
 
 
-update : () -> String -> ( String, Cmd () )
-update _ _ =
-    ( "", Cmd.none )
+update : Msg -> String -> ( String, Cmd Msg )
+update msg model =
+    case msg of
+        NoOp ->
+            ( model, Cmd.none )
+
+        DiaryBoxChange new ->
+            ( new, Cmd.none )
 
 
-view : String -> Browser.Document ()
+view : String -> Browser.Document Msg
 view model =
     { title = "DiaryVPN"
     , body = [ Element.layout [] (body model) ]
     }
 
 
-body : String -> Element ()
-body _ =
+body : String -> Element Msg
+body model =
     Element.column
         []
         [ Input.multiline
             []
-            { onChange = \_ -> ()
-            , text = ""
+            { onChange = DiaryBoxChange
+            , text = model
             , placeholder = Nothing
             , label = Input.labelAbove [] (Element.text "Type your diary entry here:")
             , spellcheck = True
