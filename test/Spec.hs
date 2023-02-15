5efbb03c829d1cf878c70ca446399728a8e171df
True Ghiassi <true@ghiassitrio.co.uk>
2023-02-15 21:36:37 +0000

    Added navigation bar to write page

--- test/Spec.hs
+++ test/Spec.hs
@@ -83,9 +83,32 @@ indexHtml =
   \      input:focus {\n\
   \        outline: blue solid 2px;\n\
   \      }\n\
+  \      a:focus {\n\
+  \        outline: blue solid 2px;\n\
+  \      }\n\
+  \      body {\n\
+  \        display: flex;\n\
+  \        flex-direction: column;\n\
+  \        row-gap: 1rem;\n\
+  \      }\n\
+  \      ul {\n\
+  \        display: flex;\n\
+  \        flex-direction: row;\n\
+  \        list-style-type: none;\n\
+  \        padding: 0;\n\
+  \        margin: 0;\n\
+  \        column-gap: 1rem;\n\
+  \        font-size: 1.2rem;\n\
+  \      }\n\
   \    </style>\n\
   \  </head>\n\
   \  <body>\n\
+  \    <nav>\n\
+  \      <ul>\n\
+  \        <li><a href=\"/read\">Read</a></li>\n\
+  \        <li><a href=\"/\">Write</a></li>\n\
+  \      </ul>\n\
+  \    </nav>\n\
   \    <form action=\"/newentry\" method=\"POST\">\n\
   \      <label for=\"newentry\">Type a new diary entry here:</label>\n\
   \      <textarea\n\
