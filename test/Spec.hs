9cd22a3a023379ad8d20def099a38ac6b9790095
True Ghiassi <true@ghiassitrio.co.uk>
2023-03-13 21:41:40 +0000

    Refactored test cases

--- test/Spec.hs
+++ test/Spec.hs
@@ -21,60 +21,76 @@ main :: IO ()
 main =
   defaultMain $ testGroup "unit tests" $ Prelude.map oneTest cases
 
-oneTest ::
-  (TestName, Strict.ByteString, Request, (Response, Strict.ByteString)) ->
-  TestTree
-oneTest (name, db, request, expected) =
-  testCase name $ diary db request @?= expected
+oneTest :: Case -> TestTree
+oneTest c =
+  testCase (description c) $
+    diary (dbIn c) (request c) @?= (response c, dbOut c)
 
-cases :: [(TestName, Strict.ByteString, Request, (Response, Strict.ByteString))]
+data Case = Case
+  { description :: TestName,
+    dbIn :: Strict.ByteString,
+    request :: Request,
+    response :: Response,
+    dbOut :: Strict.ByteString
+  }
+
+cases :: [Case]
 cases =
-  [ ( "root route",
-      "",
-      Request
-        { body = [],
-          path = [],
-          method = "GET",
-          timestamp = 0
-        },
-      ( Response
-          { body = fromStrict $ encodeUtf8 indexHtml,
-            status = ok200,
-            headers = [("Content-Type", "text/html")]
-          },
-        ""
-      )
-    ),
-    ( "favicon route",
-      "",
-      Request
-        { body = [],
-          path = ["favicon.ico"],
-          method = "GET",
-          timestamp = 0
-        },
-      ( Response
-          { body = favicon,
-            status = ok200,
-            headers = [("Content-Type", "image/vnd.microsoft.icon")]
-          },
-        ""
-      )
-    ),
-    ( "styles route",
-      "",
-      Request
-        { body = [],
-          path = ["styles.css"],
-          method = "GET",
-          timestamp = 0
-        },
-      ( Response
-          { body = fromStrict $ encodeUtf8 stylesCss,
-            status = ok200,
-            headers = [("Content-Type", "text/css")]
-          },
-        ""
-      )
-    )
+  [ Case
+      { description = "root route",
+        dbIn = "",
+        request =
+          Request
+            { body = [],
+              path = [],
+              method = "GET",
+              timestamp = 0
+            },
+        response =
+          Response
+            { body = fromStrict $ encodeUtf8 indexHtml,
+              status = ok200,
+              headers = [("Content-Type", "text/html")]
+            },
+        dbOut =
+          ""
+      },
+    Case
+      { description = "favicon route",
+        dbIn = "",
+        request =
+          Request
+            { body = [],
+              path = ["favicon.ico"],
+              method = "GET",
+              timestamp = 0
+            },
+        response =
+          Response
+            { body = favicon,
+              status = ok200,
+              headers = [("Content-Type", "image/vnd.microsoft.icon")]
+            },
+        dbOut =
+          ""
+      },
+    Case
+      { description = "styles route",
+        dbIn = "",
+        request =
+          Request
+            { body = [],
+              path = ["styles.css"],
+              method = "GET",
+              timestamp = 0
+            },
+        response =
+          Response
+            { body = fromStrict $ encodeUtf8 stylesCss,
+              status = ok200,
+              headers = [("Content-Type", "text/css")]
+            },
+        dbOut =
+          ""
+      }
   ]
