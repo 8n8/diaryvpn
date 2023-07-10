0a8a1a4bdd8f89ad3afc860a890f29a1d2adede5
True Ghiassi <true@ghiassitrio.co.uk>
2023-07-10 22:34:44 +0100

    Main function

--- main.go
+++ main.go
@@ -1,34 +1,70 @@
 package main
 
 import (
-    _ "embed"
+	_ "embed"
+	"net/http"
+	"os"
+	"sync"
 )
 
 type Request struct {
-    Header map[string]string
-    Method string
-    Path string
-    Form map[string][]string
-    Database []byte
-    DatabaseError error
+	Header        map[string][]string
+	Method        string
+	Path          string
+	Form          map[string][]string
+	Database      []byte
+	DatabaseError error
+	ParseError    error
 }
 
 type Response struct {
-    Header map[string]string
-    Status int
-    Body []byte
-    Database []byte
+	Header   map[string]string
+	Status   int
+	Body     []byte
+	Database []byte
 }
 
 //go:embed dist/index.html
 var indexHtml []byte
 
-
 func server(request Request) Response {
-    return Response{
-        Header: map[string]string{},
-        Status: 200,
-        Body: indexHtml,
-        Database: []byte(""),
-    }
+	return Response{
+		Header:   map[string]string{},
+		Status:   200,
+		Body:     indexHtml,
+		Database: []byte(""),
+	}
+}
+
+func main() {
+	var lock sync.Mutex
+
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		parseError := r.ParseForm()
+		request := Request{
+			Header:        map[string][]string(r.Header),
+			Method:        r.Method,
+			Path:          r.URL.Path,
+			ParseError:    parseError,
+			Form:          map[string][]string(r.Form),
+			Database:      []byte{},
+			DatabaseError: nil,
+		}
+
+		lock.Lock()
+		database, databaseError := os.ReadFile("database.json")
+		request.Database = database
+		request.DatabaseError = databaseError
+		response := server(request)
+		_ = os.WriteFile("database.json", response.Database, 0600)
+		lock.Unlock()
+
+		for headerKey, headerValue := range response.Header {
+			w.Header().Set(headerKey, headerValue)
+		}
+		w.WriteHeader(response.Status)
+		_, _ = w.Write(response.Body)
+	})
+
+	_ = http.ListenAndServe(":8080", nil)
 }
--- main_test.go
+++ main_test.go
@@ -1,63 +1,64 @@
 package main
 
 import (
-    "testing"
+	"testing"
 )
 
 func mapEqual(a, b map[string]string) bool {
-    if len(a) != len(b) {
-        return false
-    }
+	if len(a) != len(b) {
+		return false
+	}
 
-    for k, v := range a {
-        bv, ok := b[k]
-        if !ok {
-            return false
-        }
+	for k, v := range a {
+		bv, ok := b[k]
+		if !ok {
+			return false
+		}
 
-        if v != bv {
-            return false
-        }
-    }
+		if v != bv {
+			return false
+		}
+	}
 
-    return true
+	return true
 }
 
 func sliceEqual(a, b []byte) bool {
-    if len(a) != len(b) {
-        return false
-    }
+	if len(a) != len(b) {
+		return false
+	}
 
-    for i, item := range a {
-        if item != b[i] {
-            return false
-        }
-    }
+	for i, item := range a {
+		if item != b[i] {
+			return false
+		}
+	}
 
-    return true
+	return true
 }
 
 func responseEqual(a, b Response) bool {
-    return mapEqual(a.Header, b.Header) && a.Status == b.Status && sliceEqual(a.Body, b.Body) && sliceEqual(a.Database, b.Database)
+	return mapEqual(a.Header, b.Header) && a.Status == b.Status && sliceEqual(a.Body, b.Body) && sliceEqual(a.Database, b.Database)
 }
 
 func TestHomePage(t *testing.T) {
-    input := Request{
-        Header: map[string]string{},
-        Method: "GET",
-        Path: "",
-        Form: map[string][]string{},
-        Database: []byte(""),
-        DatabaseError: nil,
-    }
-    expected := Response{
-        Header: map[string]string{},
-        Status: 200,
-        Body: []byte(indexHtml),
-        Database: []byte(""),
-    }
-    got := server(input)
-    if !responseEqual(expected, got) {
-        t.Errorf("expecting %v but got %v", expected, got)
-    }
+	input := Request{
+		Header:        map[string][]string{},
+		Method:        "GET",
+		Path:          "",
+		Form:          map[string][]string{},
+		Database:      []byte(""),
+		DatabaseError: nil,
+		ParseError:    nil,
+	}
+	expected := Response{
+		Header:   map[string]string{},
+		Status:   200,
+		Body:     []byte(indexHtml),
+		Database: []byte(""),
+	}
+	got := server(input)
+	if !responseEqual(expected, got) {
+		t.Errorf("expecting %v but got %v", expected, got)
+	}
 }
